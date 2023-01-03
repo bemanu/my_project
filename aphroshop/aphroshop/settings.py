88573e85ce6d619139a4f255fdbf4245d96ceb17
Emmanuel Boachie <bemanub@gmail.com>
2023-01-03 21:38:38 +0000

    Initial register app

--- aphroshop/aphroshop/settings.py
+++ aphroshop/aphroshop/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'aphrobase',
-    'barbershops',
+    'person',
+    'shop',
+    'register.apps.RegisterConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
--- aphroshop/register/views.py
+++ aphroshop/register/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.isValid():
+            form.save()
+            messages.success(request,f'Your account has been created ! You are now able to log in')
+            return redirect('/homepage')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register/register.html', {'form': form})
